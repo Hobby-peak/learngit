3adbb30706c834e11ce9a150c6a0be222dec6ae7
Hobby-peak <bushi145@126.com>
2019-11-21 15:39:51 +0800

    Signed-off-by: Hobby-peak <bushi145@126.com> 0.2

--- ao-hua/ao-hua/js/section.js
+++ ao-hua/ao-hua/js/section.js
@@ -1,77 +1,104 @@
-window.onscroll=function(){
-	var scrollTop=document.documentElement.scrollTop;
-	if(scrollTop>137){
-		$("header").css("top",-137+"px");
+window.onscroll = function () {
+	var scrollTop = document.documentElement.scrollTop;
+	if (scrollTop > 137) {
+		$("header").css("top", -137 + "px");
+	} else {
+		$("header").css("top", 0 + "px");
 	}
-	else{
-		$("header").css("top",0+"px");
-	}
 }
 
-window.onload=function(){
-	var width=window.innerWidth;
-	$(".banmd").css("width",width);
-	$(".banmd>ul").css("width",5*width);
-	$(".banmd>ul>li").css("width",width);
-	$(".banmd>ul").css("margin-left",-width);
+window.onload = function () {
+	var width = window.innerWidth;
+	$(".banmd").css("width", width);
+	$(".banmd>ul").css("width", 5 * width);
+	$(".banmd>ul>li").css("width", width);
+	$(".banmd>ul").css("margin-left", -width);
 }
 
-window.onresize=function(){
-	var width=window.innerWidth;
-	$(".banmd").css("width",width);
-	$(".banmd>ul").css("width",5*width);
-	$(".banmd>ul>li").css("width",width);
-	$(".banmd>ul").css("margin-left",-width);
+window.onresize = function () {
+	var width = window.innerWidth;
+	$(".banmd").css("width", width);
+	$(".banmd>ul").css("width", 5 * width);
+	$(".banmd>ul>li").css("width", width);
+	$(".banmd>ul").css("margin-left", -width);
 }
 
-function banmd(){
-	var index=1;
-	var width=window.innerWidth;
-	var ml=-(width*index);
-	this.gothis=function(){
-		$(".banmd>ul").animate({marginLeft:ml},500,function(){
-			if(index>=4){
-				index=1;
+// function banmd() {
+// 	var index = 1;
+// 	var width = window.innerWidth;
+// 	// var ml = -(width * index);
+// 	this.gothis = function () {
+// 		// $(".banmd>ul").animate({
+// 		// 	marginLeft: ml
+// 		// }, 500)
+// 		if (index >= 4) {
+// 			index = 1;
+// 		}
+// 		if (index <= 0) {
+// 			index = 3;
+// 		}
+// 		ml = -(width * index);
+// 		setInterval(function () {
+// 			$(".banmd>ul").css("margin-left", ml + "px");
+// 		}, 500)
+// 	}
+// 	this.pven = function () {
+// 		index--;
+// 		this.gothis();
+// 	}
+// 	this.next = function () {
+// 		index++;
+// 		this.gothis();
+// 	}
+// }
+
+function baner2() {
+	var index = 2;
+	this.gotoIndex = function () {
+		var mf = -index * 1470;
+		var bActiveLeft = 0;
+		if (index < 7) {
+			bActiveLeft = (index - 2) * 22;
+			$(".active-index").text(index - 1)
+		}
+		if (index >= 7) {
+			bActiveLeft = 0; //
+			$(".active-index").text(1)
+		}
+		if (index < 2) {
+			bActiveLeft = 4 * 22;
+			$(".active-index").text(5)
+		}
+		$(".b-active").css("left", bActiveLeft + "px");
+		$(".baner2>ul").animate({
+			"marginLeft": mf + "px"
+		}, 500, function () {
+			if (index >= 7) {
+				index = 2;
 			}
-			if(index<=0){
-				index=3;
+			if (index <= 1) {
+				index = 6;
 			}
-			ml=-(width*index);
-			$(".banmd>ul").css("margin-left",ml+"px");
+			mf = -index * 1470;
+			$(".baner2>ul").css("marginLeft", mf + "px");
 		})
 	}
-	this.pven=function(){
+	this.prev = function () {
 		index--;
-		this.gothis();
-	}
-	this.next=function(){
+		this.gotoIndex()
+	};
+	this.next = function () {
 		index++;
-		this.gothis();
-	}
+		this.gotoIndex()
+	};
 }
-function banmd2(){
-	var index=2;
-	var ml=-(1470*index)
-	this.gothis=function(){
-		$(".sb-lunbo2>ul").animate({"marginLeft":ml},500,function(){
-			if(index>=7){
-				index=2;
-			}
-			if(index<=2){
-				index=6;
-			}
-			ml=-(1470*index);
-			$(".sb-lunbo2>ul").css("margin-left",ml+"px");
-		})		
-	}
-	this.pven=function(){
-		index--;
-		this.gothis();
-	}
-	this.next=function(){
-		index++;
-		this.gothis();
-	}
-}
-var b=new banmd();
-var bo=new banmd2();
+
+var b2 = new baner2();
+$(".btn-prev").click(function () {
+	b2.prev()
+})
+$(".btn-next").click(function () {
+	b2.next();
+})
+// var b = new banmd();
+// var bo = new baner2();
